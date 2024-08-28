12a99935166b7a2d1f54854c3cf8338fed082dd1
AdrianSzponarOnline <adrian.szponar7@o2.pl>
2024-08-28 13:12:05 +0200

    Added Author entity, repository and service. To manage db communication and businness logic

---
+++ src/main/java/com/librarymanagement/LibraryManagement/author/Author.java
@@ -0,0 +1,82 @@
+package com.librarymanagement.LibraryManagement.author;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "AuthorEntity", uniqueConstraints = @UniqueConstraint(columnNames = {"first_name","last_name"}))
+public class AuthorEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private long id;
+
+    @Column(name="first_name")
+    String firstName;
+    @Column(name="last_name")
+    String lastName;
+    @Column(name="nationality")
+    String nationality;
+    @Column(name="date_of_birth")
+    String dateOfBirth;
+
+    public AuthorEntity() {
+    }
+
+    public AuthorEntity(String firstName, String lastName, String nationality, String dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthorEntity{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                '}';
+    }
+}
